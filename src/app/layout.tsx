a52bec447eeaf312f213d43bb108659b034723e2
Lakshya Dhingra <dlakshya223@gmail.com>
2025-11-09 01:20:42 -0700

    Putting navbar back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Titillium_Web } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
+import Navbar from "./components/navbar";
 
 const titillium = Titillium_Web({
   weight: ["400", "700"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={titillium.className}>
         <body className="antialiased" suppressHydrationWarning>
+          <Navbar /> {/* ← Add this line */}
           <main>{children}</main>
         </body>
       </html>
